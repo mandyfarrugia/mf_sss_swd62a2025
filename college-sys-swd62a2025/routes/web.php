fb4232b5dfcc245f28a1b4a0df4b601f3eab3d54
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-03-14 16:34:05 +0000

    Defined routes related to StudentController.

--- college-sys-swd62a2025/routes/web.php
+++ college-sys-swd62a2025/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CollegeController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,3 +14,10 @@ Route::post('/colleges', [CollegeController::class, 'store'])->name('colleges.st
 Route::get('/colleges/{id}/edit', [CollegeController::class, 'edit'])->name('colleges.edit');
 Route::put('/colleges/{id}', [CollegeController::class, 'update'])->name('colleges.update');
 Route::delete('/colleges/{id}', [CollegeController::class, 'destroy'])->name('colleges.destroy');
+
+Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
+Route::delete('/students/{id}', [StudentController::class, 'destroy'])->name('students.destroy');
