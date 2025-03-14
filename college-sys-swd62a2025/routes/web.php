354ef7b60287c577efc86159f0cf029f1b1afbb0
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-03-14 16:10:01 +0000

    Defined all routes related to CollegeController.

--- college-sys-swd62a2025/routes/web.php
+++ college-sys-swd62a2025/routes/web.php
@@ -8,3 +8,8 @@ Route::get('/', function () {
 });
 
 Route::get('/colleges', [CollegeController::class, 'index'])->name('colleges.index');
+Route::get('/colleges/create', [CollegeController::class, 'create'])->name('colleges.create');
+Route::post('/colleges', [CollegeController::class, 'store'])->name('colleges.store');
+Route::get('/colleges/{id}/edit', [CollegeController::class, 'edit'])->name('colleges.edit');
+Route::put('/colleges/{id}', [CollegeController::class, 'update'])->name('colleges.update');
+Route::delete('/colleges/{id}', [CollegeController::class, 'destroy'])->name('colleges.destroy');
