6d7f53b9eccb2c5918e8da360c24e7c646ea23f0
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-03-14 14:07:03 +0000

    Defined a route to render an index page for the College controller which displays a list of all available colleges students can enroll to.

--- college-sys-swd62a2025/routes/web.php
+++ college-sys-swd62a2025/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\CollegeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/colleges', [CollegeController::class, 'index'])->name('colleges.index');
