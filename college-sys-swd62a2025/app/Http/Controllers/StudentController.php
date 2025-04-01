7f00a8618544d478b077c23f41fd7b4abee05486
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-04-01 16:34:35 +0000

    Defined logic to redirect to create page.

--- college-sys-swd62a2025/app/Http/Controllers/StudentController.php
+++ college-sys-swd62a2025/app/Http/Controllers/StudentController.php
@@ -11,10 +11,15 @@ class StudentController extends Controller
     public function index() {
         $colleges = College::orderBy('name', 'asc')->pluck('name', 'id');
         $students = Student::all();
-        return view('students.index', compact('students', 'colleges'));
+        return view('students.index', compact('colleges', 'students'));
     }
 
-    public function create() {}
+    public function create() {
+        $colleges = College::orderBy('name', 'asc')->pluck('name', 'id');
+        $student = new Student();
+        return view('students.create', compact('colleges', 'student'));
+    }
+
     public function store(Request $request) {}
     public function edit($id) {}
     public function update($id, Request $request) {}
