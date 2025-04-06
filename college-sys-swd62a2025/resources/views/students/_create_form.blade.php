7d36eb368244a1ed94d43a138ffab119e7b6c4df
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-04-06 17:12:27 +0000

    Designed partial view for create form for students

--- college-sys-swd62a2025/resources/views/students/_create_form.blade.php
+++ college-sys-swd62a2025/resources/views/students/_create_form.blade.php
@@ -2,9 +2,9 @@
     <div class="col-md-12">
         <div class="form-group">
             <label for="name">Name</label>
-            <input type="name" value="{{ old('name', $college->name) }}"
+            <input type="text" value="{{ old('name', $student->name) }}"
                 class="form-control @error('name') is-invalid @enderror" id="name"
-                placeholder="Enter the name of the college" />
+                placeholder="Enter the student's full name" name="name" />
             @error('name')
                 <div class="invalid-feedback">
                     {{ $message }}
@@ -13,16 +13,56 @@
         </div>
         <div class="form-group">
             <label for="address">Address</label>
-            <input type="address" value="{{ old('address', $college->address) }}" class="form-control @error('name') is-invalid @enderror" id="email" placeholder="Enter the address"/>
-            @error('name')
+            <input type="email" value="{{ old('email', $student->email) }}"
+                class="form-control @error('email') is-invalid @enderror" id="email"
+                placeholder="Enter an email address" name="email" />
+            @error('email')
                 <div class="invalid-feedback">
                     {{ $message }}
                 </div>
             @enderror
         </div>
         <div class="form-group">
-            <button class="btn btn-action">Back to Colleges</button>
-            <button class="btn btn-success">Submit</button>
+            <label for="phone">Phone</label>
+            <input type="text" value="{{ old('phone', $student->phone) }}"
+                class="form-control @error('phone') is-invalid @enderror" id="phone" placeholder="Enter an phone number"
+                name="phone" />
+            @error('phone')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="dob">Date of birth</label>
+            <input type="date" value="{{ old('dob', $student->dob) }}"
+                class="form-control @error('dob') is-invalid @enderror" id="dob" placeholder="Enter the date of birth"
+                name="dob" />
+            @error('dob')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="category_id">Category</label>
+            <select name="college_id" id="college_id" class="form-control @error('college_id') is-invalid @enderror"
+                name="college_id">
+                @foreach ($colleges as $id => $name)
+                    <option {{ $id == old('college_id', $student->college_id) ? 'selected' : '' }} value="{{ $id }}">
+                        {{ $name }}
+                    </option>
+                @endforeach
+            </select>
+            @error('college_id')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <a class="btn btn-action" href="{{ route('students.index') }}">Back to Colleges</a>
+            <a class="btn btn-success" href="{{ route('students.store') }}">Submit</a>
         </div>
     </div>
 </div>
