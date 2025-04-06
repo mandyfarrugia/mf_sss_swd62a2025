b44fdb5ebf267700c4e9f6a7ea472cc6d184d5d0
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-04-06 16:47:41 +0000

    Working on create view

---
+++ college-sys-swd62a2025/resources/views/students/_create_form.blade.php
@@ -0,0 +1,28 @@
+<div class="row">
+    <div class="col-md-12">
+        <div class="form-group">
+            <label for="name">Name</label>
+            <input type="name" value="{{ old('name', $college->name) }}"
+                class="form-control @error('name') is-invalid @enderror" id="name"
+                placeholder="Enter the name of the college" />
+            @error('name')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <label for="address">Address</label>
+            <input type="address" value="{{ old('address', $college->address) }}" class="form-control @error('name') is-invalid @enderror" id="email" placeholder="Enter the address"/>
+            @error('name')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+        <div class="form-group">
+            <button class="btn btn-action">Back to Colleges</button>
+            <button class="btn btn-success">Submit</button>
+        </div>
+    </div>
+</div>
