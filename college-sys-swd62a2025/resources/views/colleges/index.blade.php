231c8681486aed836385bae26fa0e79630f11c74
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-04-06 15:54:08 +0000

    Working on delete functionality via client-side

--- college-sys-swd62a2025/resources/views/colleges/index.blade.php
+++ college-sys-swd62a2025/resources/views/colleges/index.blade.php
@@ -15,38 +15,44 @@
                     </div>
                     <div class="card-body">
                         @if($colleges->count())
-                        <div class="table-responsive">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>College</th>
-                                        <th>Address</th>
-                                        <th>Actions</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($colleges as $college)
+                            <div class="table-responsive">
+                                <table class="table table-bordered">
+                                    <thead>
                                         <tr>
-                                            <td>{{ $college->name }}</td>
-                                            <td>{{ $college->address }}</td>
-                                            <td>
-                                                <div class="btn-group w-100" role="group" aria-label="Basic example">
-                                                    <a href="{{ route('colleges.show', $college->id) }}" class="btn btn-action btn-sm">
-                                                        <i class="fa-regular fa-eye"></i>
-                                                    </a>
-                                                    <a href="{{ route('colleges.edit', $college->id) }}" class="btn btn-action btn-sm">
-                                                        <i class="fa-regular fa-pen-to-square"></i>
-                                                    </a>
-                                                    <a class="btn btn-danger btn-sm">
-                                                        <i class="fa-regular fa-circle-xmark"></i>
-                                                    </a>
-                                                </div>
-                                            </td>
+                                            <th>College</th>
+                                            <th>Address</th>
+                                            <th>Actions</th>
                                         </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($colleges as $college)
+                                            <tr>
+                                                <td>{{ $college->name }}</td>
+                                                <td>{{ $college->address }}</td>
+                                                <td>
+                                                    <div class="btn-group w-100" role="group" aria-label="Basic example">
+                                                        <a href="{{ route('colleges.show', $college->id) }}"
+                                                            class="btn btn-action btn-sm">
+                                                            <i class="fa-regular fa-eye"></i>
+                                                        </a>
+                                                        <a href="{{ route('colleges.edit', $college->id) }}"
+                                                            class="btn btn-action btn-sm">
+                                                            <i class="fa-regular fa-pen-to-square"></i>
+                                                        </a>
+                                                        <a class="btn btn-danger btn-delete btn-sm">
+                                                            <i class="fa-regular fa-circle-xmark"></i>
+                                                        </a>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                        <form id="entity-delete-form" method="POST">
+                                            @method('DELETE')
+                                            @csrf
+                                        </form>
+                                    </tbody>
+                                </table>
+                            </div>
                         @else
                             <p>No colleges found!</p>
                         @endif
