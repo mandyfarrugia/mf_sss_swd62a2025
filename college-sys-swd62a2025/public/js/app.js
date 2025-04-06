c499fd6184e7563bd87a317fb2d62ea9fe7f46bb
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-04-06 16:07:39 +0000

    Fixed JS logic for deleting colleges.

---
+++ college-sys-swd62a2025/public/js/app.js
@@ -0,0 +1,21 @@
+handleButtonDeletion = (event) => {
+    event.preventDefault()
+    if (confirm("Are you sure?")) {
+        let action = this.getAttribute('href');
+        let form = document.getElementById('entity-delete-form');
+        form.setAttribute('action', action);
+        form.submit();
+    }
+}
+
+attachEvent = (target, event, callback) => {
+    target.addEventListener(event, callback);
+}
+
+attachEvent(window, 'load', () => {
+    const deleteBtns = document.querySelectorAll('.btn-delete');
+
+    deleteBtns.forEach(deleteBtn => {
+        attachEvent(deleteBtn, 'click', (event) => handleButtonDeletion(event));
+    });
+});
