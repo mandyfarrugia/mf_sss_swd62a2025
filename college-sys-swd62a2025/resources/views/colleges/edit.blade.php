93d6fbf2b27adfb58c592671737f4ebf577ff23b
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-04-06 12:50:43 +0000

    Adapted from create partial view to create edit partial view.

---
+++ college-sys-swd62a2025/resources/views/colleges/edit.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.main')
+@section('content')
+    <div class="container">
+        <div class="page-inner">
+            <div class="page-header">
+                <h3 class="fw-bold mb-3">College Management System</h3>
+                <ul class="breadcrumbs mb-3">
+                    <li class="nav-home">
+                        <a href="{{ route('colleges.index') }}">
+                            <i class="fa-solid fa-building-columns"></i>
+                            <span class="px-2">All colleges</span>
+                        </a>
+                    </li>
+                    <li class="separator">
+                        <i class="fa-solid fa-chevron-right"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">Edit college</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-title">Edit existing college</div>
+                        </div>
+                        <div class="card-body">
+                            <form action="{{ route('colleges.update') }}" method="post" enctype="multipart/form-data">
+                                @csrf
+                                @include('colleges._edit_form')
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
