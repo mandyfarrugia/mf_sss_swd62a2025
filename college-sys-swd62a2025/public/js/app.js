2deb8a4f32246aead172d3746ddecdd70ed3a291
Mandy Farrugia <mandy.farrugia.c10352@mcast.edu.mt>
2025-04-07 15:36:02 +0000

    Added more comments and preparing to push to master

--- college-sys-swd62a2025/public/js/app.js
+++ college-sys-swd62a2025/public/js/app.js
@@ -1,26 +1,28 @@
 handleNameSort = (event) => {
-    let buttonContent = event.target.innerText;
-    let sortOrder = buttonContent.includes("descending") ? 'desc' : 'asc';
-    window.location.href = window.location.href.split('?')[0] + '?name_order=' + sortOrder;
+    let buttonContent = event.target.innerText; //Get the text inside the button.
+    let sortOrder = buttonContent.includes("descending") ? 'desc' : 'asc'; //If text includes descending, sort in descending order. Otherwise sort in ascending order.
+    window.location.href = window.location.href.split('?')[0] + '?name_order=' + sortOrder; //Attach the request to sort by name in ascending/descending order.
 };
 
 handleFilterByCollegeId = (event) => {
-    let selectedCollegeId = event.target.value;
-    window.location.href = window.location.href.split('?')[0] + '?college_id=' + selectedCollegeId;
+    let selectedCollegeId = event.target.value; //Get the value associated with the option.
+    window.location.href = window.location.href.split('?')[0] + '?college_id=' + selectedCollegeId; //Attach the request to filter students by college.
 };
 
 handleButtonDeletion = (event) => {
-    event.preventDefault();
+    event.preventDefault(); //Prevent the event from being fired.
+    //Confirm whether user wants to delete the entity.
     if (confirm("Are you sure you want to delete this entity?")) {
-        let action = event.target.getAttribute('href');
-        let form = document.getElementById('entity-delete-form');
-        form.setAttribute('action', action);
-        form.submit();
+        let action = event.target.getAttribute('href'); //Get the button corresponding to the row that will be deleted.
+        let form = document.getElementById('entity-delete-form'); //Get the form to simulate form submission to cause deletion of row.
+        form.setAttribute('action', action); //Set the action to point to the destroy action.
+        form.submit(); //Simulate form submission.
     }
 };
 
 attachEvent = (target, event, callback) => target.addEventListener(event, callback);
 
+//Attach event handlers to the appropriate elements when the window has finished loading.
 attachEvent(window, 'load', () => {
     const deleteBtns = document.querySelectorAll('.btn-delete');
     const filterByCollegeId = document.getElementById('filter_college_id');
